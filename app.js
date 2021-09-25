90805e8c646aefc9ce15d1a62966d0dd0161f4d2
sonam23Pathak <76949052+sonam23Pathak@users.noreply.github.com>
2021-09-25 23:34:48 +0530

    style: styled using html, css

---
+++ app.js
@@ -0,0 +1,28 @@
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const output = document.querySelector("#output");
+
+const serverURL = "https://api.funtranslations.com/translate/yoda.json";
+
+function getTranslationURL(text) {
+
+    return serverURL + "?" + "text=" + text
+
+}
+
+function clickHandler() {
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+
+            var translatedText = json.contents.translated;
+            output.innerText = translatedText;
+            console.log(json.contents.translated)
+        })
+
+};
+
+btnTranslate.addEventListener("click", clickHandler)
